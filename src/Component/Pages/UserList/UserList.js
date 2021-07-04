ed85e766f68d7f3ca3bf93dc8dcf836a0b7ad3ee
Sarman7105 <sarman7105@gmail.com>
2021-07-04 15:56:48 +0600

    react router dom package added

---
+++ src/Component/Pages/UserList/UserList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserList.css'
+
+const UserList = () => {
+    return (
+        <div className="userList">
+            UserList
+        </div>
+    );
+};
+
+export default UserList;
