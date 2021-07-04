b5d5a27c14b9c68cc5b65c7fa1bf08700efebf6e
Sarman7105 <sarman7105@gmail.com>
2021-07-04 15:51:18 +0600

    home page for admin panel added

--- src/Component/Pages/Home/Home.js
+++ src/Component/Pages/Home/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Chart from '../../Chart/Chart';
 import FeaturedInfo from '../../FeaturedInfo/FeaturedInfo';
-import './Home.css'
+import './Home.css';
+import { userData } from '../../../DummyData';
+import WidgetSm from '../../../Component/WidgetSm/WidgetSm';
+import WidgetLg from '../../../Component/WidgetLg/WidgetLg';
 const Home = () => {
-    return (
-        <div className="home">
-            <FeaturedInfo/>
-        </div>
-    );
+	return (
+		<div className="home">
+			<FeaturedInfo />
+			<Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+			<div className="homeWidgets">
+				<WidgetSm />
+				<WidgetLg />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
